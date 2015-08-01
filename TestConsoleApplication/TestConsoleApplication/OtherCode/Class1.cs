0e4c033636716f0966a92baf6cf20d5796f0132f
Juiceek <alexey.prostak@gmail.com>
2015-08-01 19:12:36 +0300

    Creation

---
+++ TestConsoleApplication/TestConsoleApplication/OtherCode/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SCG = System.Collections.Generic;
+
+namespace TestConsoleApplication
+{
+    class Item 
+    {
+        public System.DateTime DateValue {get; set;}
+        public System.DateTime TimeValue {get; set;}
+        public System.TimeSpan TimeInterval {get; set;}
+
+        public Item(DateTime date)
+        {
+            this.DateValue = date;
+        }
+    }
+
+    class ItemComparer : SCG.IComparer<Item>
+    {
+        int IComparer<Item>.Compare(Item x, Item y)
+        {
+            return 0;
+        }
+    }
+
+
+    static class MainClass
+    {
+        public static void MainMethod()
+        {
+            
+        }
+
+        public static void PrintList(IEnumerable collection)
+        {
+            Console.Write("\n");
+
+            bool first = true;
+            foreach (object element in collection)
+            {
+                if (!first)
+                {
+                    Console.Write(',');
+                }
+                else
+                {
+                    first = false;
+                }
+                Console.Write("[{0}]", element);
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ TestConsoleApplication/TestConsoleApplication/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    struct Struct2
+    {
+        //sbyte sbField;
+
+        const ulong MYCONST = 2;
+    }
+
+
+    //---------------------------------
+    enum MyEnum : byte
+    {
+        None = 1,
+        A = 2,
+        B = 3,
+        C = 4
+    }
+
+    class TestEnum
+    {
+        public MyEnum e;
+    }
+    //----------------------
+
+
+
+
+    struct MyStruct
+    {
+        internal byte x;
+        internal byte y;
+
+        public MyStruct(char c = 'q')
+        {
+            Console.WriteLine("in the constructor, char c is '{0}' ", c);
+            //
+            this.x = 1;
+            this.y = 1;
+        }
+
+        public override string ToString()
+        {
+            return 
+                base.ToString() + " ====== " +
+                    String.Format("{{ x={0}, y={1} }}", x, y);
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            items.Add(new Item(System.DateTime.Now));
+            items.Add(new Item(System.DateTime.Now));
+            items.Add(new Item(System.DateTime.Now));
+
+            MainClass.PrintList( items);
+
+            items.Sort();
+            
+            //--------------------------------------------------
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
